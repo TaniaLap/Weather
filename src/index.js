cb65b60bb53c9730346ca994a34f70a34d33a781
TaniaLap <112969211+TaniaLap@users.noreply.github.com>
2022-09-11 21:27:37 +0200

    Display the forecast data using the API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// This function formats the date and the hour
+
 function formatDate(date) {
   let hours = date.getHours();
   if (hours < 10) {
@@ -26,7 +28,100 @@ let dateElement = document.querySelector("#nowday");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
 
-// The weather
+// This function formats the day in the forecast columns
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+  return days[day];
+}
+
+// This function change the weather image from the medias folder
+
+function displayImage(icon) {
+  let iconPath = "";
+  if (icon === `01d`) {
+    iconPath = "./images/sun.png";
+  } else if (icon === `02d`) {
+    iconPath = "./images/warm.png";
+  } else if (
+    icon === `03d` ||
+    icon === `04d` ||
+    icon === `03n` ||
+    icon === `04n`
+  ) {
+    iconPath = "./images/clouds.png";
+  } else if (
+    icon === `09d` ||
+    icon === `09n` ||
+    icon === `10d` ||
+    icon === `10n`
+  ) {
+    iconPath = "./images/rain.png";
+  } else if (icon === `11d` || icon === `11n`) {
+    iconPath = "./images/storm.png";
+  } else if (icon === `13d` || icon === `13n`) {
+    iconPath = "./images/snow.png";
+  } else if (icon === `50d` || icon === `50n`) {
+    iconPath = "./images/mist.png";
+  } else if (icon === `01n`) {
+    iconPath = "./images/moon.png";
+  } else if (icon === `02n`) {
+    iconPath = "./images/moonclouds.png";
+  }
+
+  return iconPath;
+}
+
+// This function display the forecast COLUMNS
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row row-cols-2 row-cols-md-3 row-cols-lg-6 p-4 pt-0 m-0 g-2">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+<div class="col-2 ">
+    <div class="card text-center h-100">
+          <img src="${displayImage(forecastDay.weather[0].icon)}"
+            alt=""
+             class="mx-auto d-block mt-4"
+             id="icon"/>
+      <div class="card-body pt-1">
+        <h5 class="card-title m-0">
+          <span class="weather-forecast-temperature-max">${Math.round(
+            forecastDay.temp.max
+          )}°</span>
+          <span class="weather-forecast-temperature-min">${Math.round(
+            forecastDay.temp.min
+          )}°</span>
+         </h5>
+         <p class="card-text">${formatDay(forecastDay.dt)}</p>
+</div>
+    </div>
+      </div>`;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+// This fonction get the coordinates
+
+function getForecast(coordinates) {
+  let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+// This fonction get the API reponse and dispatches it on HTML
 
 function showTemperature(response) {
   document.querySelector("#temperature").innerHTML = Math.round(
@@ -46,54 +141,16 @@ function showTemperature(response) {
     response.data.main.temp_max
   )}°C`;
 
-  let iconElementAPI = response.data.weather[0].icon;
-
-  if (iconElementAPI === "01d") {
-    document.querySelector("#icon").setAttribute("src", `./images/sun.png`);
-  } else if (iconElementAPI === "02d") {
-    document.querySelector("#icon").setAttribute("src", `./images/warm.png`);
-  } else if (iconElementAPI === "03d") {
-    document.querySelector("#icon").setAttribute("src", `./images/clouds.png`);
-  } else if (iconElementAPI === "04d") {
-    document.querySelector("#icon").setAttribute("src", `./images/clouds.png`);
-  } else if (iconElementAPI === "09d") {
-    document.querySelector("#icon").setAttribute("src", `./images/rain.png`);
-  } else if (iconElementAPI === "10d") {
-    document.querySelector("#icon").setAttribute("src", `./images/rain.png`);
-  } else if (iconElementAPI === "11d") {
-    document.querySelector("#icon").setAttribute("src", `./images/storm.png`);
-  } else if (iconElementAPI === "13d") {
-    document.querySelector("#icon").setAttribute("src", `./images/snow.png`);
-  } else if (iconElementAPI === "50d") {
-    document.querySelector("#icon").setAttribute("src", `./images/mist.png`);
-  } else if (iconElementAPI === "01n") {
-    document.querySelector("#icon").setAttribute("src", `./images/moon.png`);
-  } else if (iconElementAPI === "02n") {
-    document
-      .querySelector("#icon")
-      .setAttribute("src", `./images/moonclouds.png`);
-  } else if (iconElementAPI === "03n") {
-    document.querySelector("#icon").setAttribute("src", `./images/clouds.png`);
-  } else if (iconElementAPI === "04n") {
-    document.querySelector("#icon").setAttribute("src", `./images/clouds.png`);
-  } else if (iconElementAPI === "09n") {
-    document.querySelector("#icon").setAttribute("src", `./images/rain.png`);
-  } else if (iconElementAPI === "10n") {
-    document.querySelector("#icon").setAttribute("src", `./images/rain.png`);
-  } else if (iconElementAPI === "11n") {
-    document.querySelector("#icon").setAttribute("src", `./images/storm.png`);
-  } else if (iconElementAPI === "13n") {
-    document.querySelector("#icon").setAttribute("src", `./images/snow.png`);
-  } else if (iconElementAPI === "50n") {
-    document.querySelector("#icon").setAttribute("src", `./images/mist.png`);
-  }
-
-  document
-    .querySelector("#icon")
-    .setAttribute("alt", response.data.weather[0].description);
+  let image = document.querySelector("#icon");
+  let icon = response.data.weather["0"].icon;
 
   celsiusTemperature = response.data.main.temp;
+  getForecast(response.data.coord);
+  image.setAttribute("src", displayImage(icon));
 }
+
+// City
+
 function handleSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#search-text-input").value;
@@ -116,6 +173,9 @@ function showPosition(position) {
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
   axios.get(url).then(showTemperature);
 }
+
+// -------current-location-button-----------
+
 function retrievePosition(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(showPosition);
@@ -123,6 +183,8 @@ function retrievePosition(event) {
 
 let buttonCurrent = document.querySelector("#current-location-button");
 buttonCurrent.addEventListener("click", retrievePosition);
+
+// -----Fahrenheit-------------
 
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
@@ -133,6 +195,8 @@ function displayFahrenheitTemperature(event) {
   let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
 }
+
+// -------Celsius-----------
 
 function displayCelsiusTemperature(event) {
   event.preventDefault();
